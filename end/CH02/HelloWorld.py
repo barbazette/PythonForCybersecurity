6bde0cb680405c195c9ff1b8303d261b46af35c5
barbazette <dwb0017@mix.wvu.edu>
2022-08-29 16:02:51 -0400

    In class: August 29, 2022

--- end/CH02/HelloWorld.py
+++ end/CH02/HelloWorld.py
@@ -2,3 +2,5 @@
 # A simple "Hello World" script in python
 # Created by Ed Goad, 2/3/2021
 print("Hello World")
+print('Python reads code line by line')
+print("These just save locally, need to push to github")
